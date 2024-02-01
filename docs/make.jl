0503ee9bd1fce986702e55c263d66e78dabcb754
Mason Protter <mason.protter@icloud.com>
2024-02-01 14:53:30 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,6 @@ function should_push_preview(event_path = get(ENV, "GITHUB_EVENT_PATH", nothing)
     pull_request = get(event, "pull_request", nothing)
     pull_request === nothing && return false
     labels = [x["name"] for x in pull_request["labels"]]
-    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
     yes = "push_preview" in labels
     if yes
         @info "Trying to push preview as label `push_preview` is specified." labels
@@ -25,7 +24,6 @@ makedocs(;
         "Home" => "index.md",
     ],
     warnonly = true,
-    # repo="https://github.com/tkf/ThreadsX.jl/blob/{commit}{path}#L{line}",
     sitename="ThreadsX.jl",
     authors="Takafumi Arakaki <aka.tkf@gmail.com>",
 )
